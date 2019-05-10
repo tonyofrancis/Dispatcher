13e3cdd480c6cd21546b1a6a0bb1259cc2d9e376
Tonyo Francis <tonyo.f@irokopartners.com>
2019-05-10 14:14:51 -0400

    Added easy queue access properties

---
+++ dispatch/src/main/java/com/tonyodev/dispatch/Queues.kt
@@ -0,0 +1,37 @@
+@file:JvmName("DispatchQueues")
+package com.tonyodev.dispatch
+
+/**
+ * Creates a new dispatch queue that can be used to post work on the main thread or do work in the background.
+ * The dispatch queue operates on the default background handler/thread.
+ * @return new dispatch.
+ * */
+val backgroundDispatchQueue = Dispatcher.backgroundDispatchQueue
+
+/**
+ * Creates a new dispatch queue that can be used to post work on the main thread or do work in the background.
+ * The dispatch queue operates on the secondary background handler/thread.
+ * @return new dispatch.
+ * */
+val backgroundSecondaryDispatchQueue = Dispatcher.backgroundSecondaryDispatchQueue
+
+/**
+ * Creates a new dispatch queue that can be used to post work on the main thread or do work in the background.
+ * The dispatch queue operates on the default network handler/thread.
+ * @return new dispatch.
+ * */
+val networkDispatchQueue = Dispatcher.networkDispatchQueue
+
+/**
+ * Creates a new dispatch queue that can be used to post work on the main thread or do work in the background.
+ * The dispatch queue operates on the default io handler/thread.
+ * @return new dispatch.
+ * */
+val ioDispatchQueue = Dispatcher.ioDispatchQueue
+
+/**
+ * Creates a new test dispatch queue. All async and post run on the same thread this dispatch was created on.
+ * Note: Test Dispatch queues do not run with delays.
+ * @return test dispatch.
+ * */
+val testDispatchQueue = Dispatcher.testDispatchQueue
