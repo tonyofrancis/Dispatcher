041f6b5b870ce680b5c71fbfd473857a089c230c
Tonyo Francis <tonyo.f@irokopartners.com>
2019-05-10 09:51:55 -0400

    Improvements to threader for lazy loading

--- dispatch/src/main/java/com/tonyodev/dispatch/utils/Threader.kt
+++ dispatch/src/main/java/com/tonyodev/dispatch/utils/Threader.kt
@@ -10,41 +10,31 @@ internal object Threader {
     @Volatile
     private var newThreadCount = 0
 
-    val uiHandler = Handler(Looper.getMainLooper())
+    val uiHandler by lazy { Handler(Looper.getMainLooper()) }
 
-    val backgroundHandler: Handler = {
+    val backgroundHandler: Handler by lazy {
         val handlerThread = HandlerThread("dispatcherBackground")
         handlerThread.start()
         Handler(handlerThread.looper)
-    }()
+    }
 
-    var backgroundSecondaryHandler: Handler? = null
-        get() {
-            if (field == null) {
-                val handlerThread = HandlerThread("dispatcherBackgroundSecondary")
-                handlerThread.start()
-                field = Handler(handlerThread.looper)
-            }
-            return field
-        }
-    var networkHandler: Handler? = null
-        get() {
-            if (field == null) {
-                val handlerThread = HandlerThread("dispatcherNetwork")
-                handlerThread.start()
-                field = Handler(handlerThread.looper)
-            }
-            return field
-        }
-    var ioHandler: Handler? = null
-        get() {
-            if (field == null) {
-                val handlerThread = HandlerThread("dispatcherIO")
-                handlerThread.start()
-                field = Handler(handlerThread.looper)
-            }
-            return field
-        }
+    private val backgroundSecondaryHandler: Handler by lazy {
+        val handlerThread = HandlerThread("dispatcherBackgroundSecondary")
+        handlerThread.start()
+        Handler(handlerThread.looper)
+    }
+
+    private val networkHandler: Handler by lazy {
+        val handlerThread = HandlerThread("dispatcherNetwork")
+        handlerThread.start()
+        Handler(handlerThread.looper)
+    }
+
+    private val ioHandler: Handler by lazy {
+        val handlerThread = HandlerThread("dispatcherIO")
+        handlerThread.start()
+        Handler(handlerThread.looper)
+    }
 
     fun getNewDispatchHandler(name: String? = null): Handler {
         val threadName = if (name == null || name.isEmpty()) {
@@ -57,13 +47,23 @@ internal object Threader {
         return Handler(handlerThread.looper)
     }
 
+    private val backgroundThreadPair: Pair<Handler, Boolean> by lazy { Pair(backgroundHandler, false) }
+
+    private val backgroundSecondaryThreadPair: Pair<Handler, Boolean> by lazy { Pair(backgroundSecondaryHandler, false) }
+
+    private val networkThreadPair: Pair<Handler, Boolean> by lazy { Pair(networkHandler, false) }
+
+    private val uiThreadPair: Pair<Handler, Boolean> by lazy { Pair(uiHandler, false) }
+
+    private val ioThreadPair: Pair<Handler, Boolean> by lazy { Pair(ioHandler, false) }
+
     fun getHandlerPairForThreadType(threadType: ThreadType): Pair<Handler, Boolean> {
         return when(threadType) {
-            ThreadType.BACKGROUND -> Pair(backgroundHandler, false)
-            ThreadType.BACKGROUND_SECONDARY -> Pair(backgroundSecondaryHandler!!, false)
-            ThreadType.IO -> Pair(ioHandler!!, false)
-            ThreadType.NETWORK -> Pair(networkHandler!!, false)
-            ThreadType.MAIN -> Pair(uiHandler, false)
+            ThreadType.BACKGROUND -> backgroundThreadPair
+            ThreadType.BACKGROUND_SECONDARY -> backgroundSecondaryThreadPair
+            ThreadType.IO -> ioThreadPair
+            ThreadType.NETWORK -> networkThreadPair
+            ThreadType.MAIN -> uiThreadPair
             ThreadType.NEW -> Pair(getNewDispatchHandler(), true)
         }
     }
