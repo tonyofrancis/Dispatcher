33507f6c5a5f806bd2f090372dee6fbd182f56fb
Tonyo Francis <tonyo.f@irokopartners.com>
2019-05-11 19:39:51 -0400

    fixes to queues

--- dispatch/src/main/java/com/tonyodev/dispatch/Queues.kt
+++ dispatch/src/main/java/com/tonyodev/dispatch/Queues.kt
@@ -6,32 +6,47 @@ package com.tonyodev.dispatch
  * The dispatch queue operates on the default background handler/thread.
  * @return new dispatch.
  * */
-val backgroundDispatchQueue = Dispatcher.backgroundDispatchQueue
+val backgroundDispatchQueue: Dispatch<Unit>
+    get() {
+        return Dispatcher.backgroundDispatchQueue
+    }
 
 /**
  * Creates a new dispatch queue that can be used to post work on the main thread or do work in the background.
  * The dispatch queue operates on the secondary background handler/thread.
  * @return new dispatch.
  * */
-val backgroundSecondaryDispatchQueue = Dispatcher.backgroundSecondaryDispatchQueue
+val backgroundSecondaryDispatchQueue: Dispatch<Unit>
+    get() {
+        return Dispatcher.backgroundSecondaryDispatchQueue
+    }
 
 /**
  * Creates a new dispatch queue that can be used to post work on the main thread or do work in the background.
  * The dispatch queue operates on the default network handler/thread.
  * @return new dispatch.
  * */
-val networkDispatchQueue = Dispatcher.networkDispatchQueue
+val networkDispatchQueue: Dispatch<Unit>
+    get() {
+        return Dispatcher.networkDispatchQueue
+    }
 
 /**
  * Creates a new dispatch queue that can be used to post work on the main thread or do work in the background.
  * The dispatch queue operates on the default io handler/thread.
  * @return new dispatch.
  * */
-val ioDispatchQueue = Dispatcher.ioDispatchQueue
+val ioDispatchQueue: Dispatch<Unit>
+    get() {
+        return Dispatcher.ioDispatchQueue
+    }
 
 /**
  * Creates a new test dispatch queue. All async and post run on the same thread this dispatch was created on.
  * Note: Test Dispatch queues do not run with delays.
  * @return test dispatch.
  * */
-val testDispatchQueue = Dispatcher.testDispatchQueue
+val testDispatchQueue: Dispatch<Unit>
+    get() {
+        return Dispatcher.testDispatchQueue
+    }
