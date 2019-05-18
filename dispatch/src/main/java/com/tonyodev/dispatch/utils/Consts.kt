a0da4d48c52888b4253a29f52ff781a492fb235d
Tonyo Francis <tonyo.f@irokopartners.com>
2019-05-18 13:22:40 -0400

    removed thread name

--- dispatch/src/main/java/com/tonyodev/dispatch/utils/Consts.kt
+++ dispatch/src/main/java/com/tonyodev/dispatch/utils/Consts.kt
@@ -3,7 +3,6 @@ package com.tonyodev.dispatch.utils
 const val TAG = "DispatchQueue"
 internal val INVALID_RESULT = InvalidResult()
 const val THREAD_BACKGROUND = "DispatchQueueBackground"
-const val THREAD_BACKGROUND_SECONDARY = "DispatchQueueBackgroundSecondary"
 const val THREAD_IO = "DispatchQueueIO"
 const val THREAD_TEST = "DispatchQueueTest"
 const val THREAD_MAIN_NO_UI = "DispatchQueueMain"
