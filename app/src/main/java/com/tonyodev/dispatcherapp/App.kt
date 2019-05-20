67f9798e3b1dfad9efcd834ce347d01987268ed1
Tonyo Francis <tonyo.f@irokopartners.com>
2019-05-20 09:23:55 -0400

    removed library

--- app/src/main/java/com/tonyodev/dispatcherapp/App.kt
+++ app/src/main/java/com/tonyodev/dispatcherapp/App.kt
@@ -1,13 +1,11 @@
 package com.tonyodev.dispatcherapp
 
 import android.app.Application
-import leakcanary.LeakSentry
 
 class App: Application() {
 
     override fun onCreate() {
         super.onCreate()
-        LeakSentry.manualInstall(this)
     }
 
 }
