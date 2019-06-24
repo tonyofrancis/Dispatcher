705685a4c49b8e05fff7815787f5d9b97bbc3c35
Tonyo Francis <tonyo.f@irokopartners.com>
2019-06-24 16:55:14 -0400

    error message updates

--- dispatch/src/main/java/com/tonyodev/dispatch/utils/Utils.kt
+++ dispatch/src/main/java/com/tonyodev/dispatch/utils/Utils.kt
@@ -43,7 +43,7 @@ internal fun throwIllegalStateExceptionIfStarted(dispatchQueueInfo: DispatchQueu
 
 internal fun throwIllegalArgumentExceptionIfListEmpty(list: List<Any?>) {
     if (list.isEmpty()) {
-        throw IllegalArgumentException("Cannot pass in an empty list to be zipped.")
+        throw IllegalArgumentException("Cannot pass in an empty list.")
     }
 }
 
