65f8402bf1e003306023988e4d4be267a3db568b
Tonyo Francis <tonyo.f@irokopartners.com>
2019-06-25 12:05:21 -0400

    Added test file

---
+++ dispatch/src/test/java/com/tonyodev/dispatch/DispatchQueueObservableTest.kt
@@ -0,0 +1,37 @@
+package com.tonyodev.dispatch
+
+import org.junit.Test
+
+class DispatchQueueObservableTest {
+
+    @Test
+    fun testAddObserver() {
+
+    }
+
+    @Test
+    fun testRemoveObserver() {
+
+    }
+
+    @Test
+    fun testObserverSize() {
+
+    }
+
+    @Test
+    fun testThreadHandler() {
+
+    }
+
+    @Test
+    fun testShouldNotify() {
+
+    }
+
+    @Test
+    fun testNotify() {
+
+    }
+
+}
